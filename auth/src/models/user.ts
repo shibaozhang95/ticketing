e94c82d4fc229da1ae38817fc3d9d5740dacf56c
BobyZhang <zsb0213@gmail.com>
2020-09-15 12:07:12 +1000

    finished auth, routes and thier related middlewares

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -21,16 +21,27 @@ interface UserDoc extends mongoose.Document {
   password: string;
 }
 
-const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true
-  },
-  password: {
-    type: String,
-    required: true
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true
+    },
+    password: {
+      type: String,
+      required: true
+    }, 
+  }, {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.password;
+        delete ret.__v;
+      }
+    }
   }
-});
+);
 
 // mongoose middleware
 userSchema.pre('save', async function(done) {
